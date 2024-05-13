b4a215fd88d15731d862d23d2a11d0680f522700
James Boland <jamespatrickboland@gmail.com>
2024-05-13 08:15:32 +0200

    Day 2 part 2 complete

--- day_02/cube_conundrum_p2.py
+++ day_02/cube_conundrum_p2.py
@@ -1,30 +1,27 @@
 from argparse import ArgumentParser
+from math import prod
 from pathlib import Path
+
+
 
 from utils.io_utils import read_file_by_line
 
 parser = ArgumentParser(prog="cube-conundrum-1")
 
 parser.add_argument("input_name")
-parser.add_argument("--rule", "-r", default="r12,g13,b14")
 
-def cube_conundrum_1(input_name, rule_str="r12,g13,b14"):
-    rule = {
-        colour_rule[0]: int(colour_rule[1:])
-        for colour_rule in rule_str.split(",") 
-    }
-
+def cube_conundrum_2(input_name):
     path = Path(__file__).parent / input_name
     input_lines = read_file_by_line(path)
 
     possible_ids = [
-        return_id_if_possible(line, rule)
+        calculate_game_power(line)
         for line in input_lines
     ]
     return sum(possible_ids)
 
 
-def return_id_if_possible(line_str, rule):
+def calculate_game_power(line_str):
     gid, results = parse_game(line_str)
     all_keys = set().union(*[result.keys() for result in results])
 
@@ -32,11 +29,7 @@ def return_id_if_possible(line_str, rule):
         key: max([result.get(key, 0) for result in results])
         for key in all_keys
     }
-    ok = [
-        1 - int(key in rule.keys() and max_values[key] <= rule[key])
-        for key in all_keys
-    ]
-    return gid if sum(ok) == 0 else 0
+    return prod(max_values.values())
 
 def parse_game(line):
     game_str, results_str = line.split(": ")
@@ -57,5 +50,5 @@ def parse_round_result(result_str):
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    answer = cube_conundrum_1(args.input_name, rule_str=args.rule)
+    answer = cube_conundrum_2(args.input_name)
     print(answer)
